ed1c0ad2ddb5556bba860bff6d740f2b29dbb935
Lars Kemper <l.kemper@shopware.com>
2025-05-08 11:11:57 +0200

    fix: nav links

--- src/components/navigation.php
+++ src/components/navigation.php
@@ -1,15 +1,15 @@
 <header>
   <div class="container">
     <div class="logo">
-      <a href="/src/index.php">Poster CMS</a>
+      <a href="/ss25-www1/src/index.php">Poster CMS</a>
     </div>
     <nav>
       <ul>
-        <li><a href="/src/index.php">Overview</a></li>
-        <li><a href="/src/pages/account.php">Account</a></li>
-        <li><a href="/src/pages/login.php">Login</a></li>
-        <li><a href="/src/pages/poster-designer.php">Poster designer</a></li>
-        <li><a href="/src/pages/media.php">Media</a></li>
+        <li><a href="/ss25-www1/src/index.php">Overview</a></li>
+        <li><a href="/ss25-www1/src/pages/account.php">Account</a></li>
+        <li><a href="/ss25-www1/src/pages/login.php">Login</a></li>
+        <li><a href="/ss25-www1/src/pages/poster-designer.php">Poster designer</a></li>
+        <li><a href="/ss25-www1/src/pages/media.php">Media</a></li>
       </ul>
     </nav>
   </div>
--- src/shared/file-path-enum.php
+++ src/shared/file-path-enum.php
@@ -14,14 +14,14 @@ enum FilePathEnum
   public function getPath(): string
   {
     return match ($this) {
-      self::HOME => '/index.php',
-      self::ACCOUNT => '/pages/account.php',
-      self::DETAIL_VIEW => '/pages/detail-view.php',
-      self::MEDIA => '/pages/media.php',
-      self::POSTER_DESIGNER => '/pages/poster-designer.php',
-      self::LOGIN => '/pages/login.php',
-      self::REGISTER => '/pages/register.php',
-      self::NOT_FOUND => '/pages/not-found.php',
+      self::HOME => '/ss25-www1/src/index.php',
+      self::ACCOUNT => '/ss25-www1/src/pages/account.php',
+      self::DETAIL_VIEW => '/ss25-www1/src/pages/detail-view.php',
+      self::MEDIA => '/ss25-www1/src/pages/media.php',
+      self::POSTER_DESIGNER => '/ss25-www1/src/pages/poster-designer.php',
+      self::LOGIN => '/ss25-www1/src/pages/login.php',
+      self::REGISTER => '/ss25-www1/src/pages/register.php',
+      self::NOT_FOUND => '/ss25-www1/src/pages/not-found.php',
     };
   }
 }
