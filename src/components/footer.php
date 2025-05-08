6676cb440e063b3a68af397107bce7a81f2e6eb0
Tom Steinbach <tostein04@gmail.com>
2025-05-08 09:37:46 +0200

    feat: added footer in php

---
+++ src/components/footer.php
@@ -0,0 +1,24 @@
+<footer>
+      <div class="container">
+        <div>
+          <p>&copy; 2025 FH Münster - Labor für Softwarearchitektur</p>
+          <p>Creative Technologies Lab - Web Development I</p>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="">Terms of Use</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
--- src/pages/register.php
+++ src/pages/register.php
@@ -56,29 +56,8 @@
       </section>
     </main>
 
-    <footer>
-      <div class="container">
-        <div>
-          <p>&copy; 2025 FH Münster - Labor für Softwarearchitektur</p>
-          <p>Creative Technologies Lab - Web Development I</p>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href="">About</a>
-            </li>
-            <li>
-              <a href="">Privacy Policy</a>
-            </li>
-            <li>
-              <a href="">Terms of Use</a>
-            </li>
-            <li>
-              <a href="">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </footer>
+    <?php
+    require  __DIR__."/../components/footer.php";
+    ?>
   </body>
 </html>
